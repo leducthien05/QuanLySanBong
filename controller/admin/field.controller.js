62aa8513f1f0cc43013a63635a8249832d8d6fec
le Duc Thien <ducthien09092005@gmail.com>
2026-05-09 22:21:18 +0700

    Tạo giao diện trang danh sách sân

---
+++ controller/admin/field.controller.js
@@ -0,0 +1,6 @@
+
+module.exports.index = async (req, res)=>{
+    res.render("admin/page/field/index", {
+        titlePage: "Sân"
+    });
+}
--- router/admin/index.router.js
+++ router/admin/index.router.js
@@ -1,8 +1,10 @@
 const dashboardRouter = require("./dashboard.router");
+const fieldRouter = require("./field.router");
 
 const systemConfig = require("../../config/system");
 
 module.exports = (app) =>{
     const prefixAdmin = systemConfig.systemConfig.prefixAdmin;
     app.use(prefixAdmin + "/dashboard", dashboardRouter);
+    app.use(prefixAdmin + "/fields", fieldRouter);
 }
